ba26235721a697a3806f4c72818e1cdf24d01600
Eric Toribio <91295488+EricToribio@users.noreply.github.com>
2021-11-12 21:30:41 -0500

    dojoPizza complete

---
+++ JS/dojoPizzeria.js
@@ -0,0 +1,37 @@
+var pizza = {
+    crustType: ["hand tossed", "chicago deep dish", "new york style", "thin crust"],
+    sauceType: ["traditional", "spicey", "marinara"],
+    cheeses: ["mozzarella", "feta", "swiss", "peperjack", "guda"],
+    toppings: ["pineapple", "peperoni", "sausage", "bacon", "black olives", "ham"]
+}
+function randomPizza(){
+    var pizza = {
+        crustType: ["hand tossed", "chicago deep dish", "new york style", "thin crust"],
+        sauceType: ["traditional", "spicey", "marinara"],
+        cheeses: ["mozzarella", "feta", "swiss", "peperjack", "guda"],
+        toppings: ["pineapple", "peperoni", "sausage", "bacon", "black olives", "ham"]
+    }
+    pizza = {
+          crustType:   pizza.crustType[Math.floor(Math.random() * pizza.crustType.length)],
+        sauceType:  pizza.sauceType[Math.floor(Math.random() * pizza.sauceType.length)],
+        cheeses:  pizza.cheeses[Math.floor(Math.random() * pizza.cheeses.length)],
+        toppings:   pizza.toppings[Math.floor(Math.random() * pizza.toppings.length)]
+    }
+    return pizza
+}
+console.log(randomPizza())
+
+
+
+function pizzaOven(crustType = "deep dish", sauceType = "traditional", cheeses = "mozzarella", toppings = ["peperoni", "sausage"]) {
+    pizza = {
+        crustType: crustType,
+        sauceType: sauceType,
+        cheeses: cheeses,
+        toppings: toppings
+    }
+    return pizza
+}
+console.log(pizzaOven())
+console.log(pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]))
+console.log(pizzaOven("new york style", "traditional", "mozzarella", ["peperoni", "bacon", "black olives", "pineapple", "ham"]))
